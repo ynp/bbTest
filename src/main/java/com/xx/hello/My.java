abbe03df1ab30849ec7b854d385caa180829d8de
ynp <ynp507@.com>
2019-06-12 15:56:01 +0800

    加入测试类

---
+++ src/main/java/com/xx/hello/My.java
@@ -0,0 +1,16 @@
+/**
+ * Project Name:bbTest
+ * File Name:My.java
+ * Package Name:com.xx.hello
+ * Date:2019年6月12日下午3:55:27
+ * Copyright (c) 2019, SPRIXIN.com All Rights Reserved.
+ */
+package com.xx.hello;
+
+/**这里用一句话描述这个类的作用<br/><br/>
+ * 创建时间： 2019年6月12日 下午3:55:27 <br/>
+ * @author 
+ */
+public class My {
+
+}
